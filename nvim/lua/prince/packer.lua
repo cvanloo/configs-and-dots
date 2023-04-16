f01316808ec15a5fb3e580c75b4589d06f04b91a
Colin van Loo <colin.vanloo@edu.tbz.ch>
2023-04-16 18:16:47 +0200

    nvim: automatically install packer

--- nvim/lua/prince/packer.lua
+++ nvim/lua/prince/packer.lua
@@ -1,4 +1,15 @@
-vim.cmd [[packadd packer.nvim]]
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
+end
+
+local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
     -- Packer can manage itself
@@ -49,4 +60,10 @@ return require('packer').startup(function(use)
     use { 'bakpakin/janet.vim' }
 
     use 'sjl/badwolf'
+
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end)
